f5b66c8654a86c719b21fb3a3571725731a78817
Disco Turkey <gelsinon.gwen@gmail.com>
2019-08-08 10:08:06 -0600

    completed step 5

---
+++ app/data/friends.js
@@ -0,0 +1,134 @@
+// {
+//     "name":"Ahmed",
+//     "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+//     "scores":[
+//         5,
+//         1,
+//         4,
+//         4,
+//         5,
+//         1,
+//         2,
+//         5,
+//         4,
+//         1
+//       ]
+//   }
+var friends = [
+    {
+        name: "Topaz",
+        photo: "https://ca.slack-edge.com/TEKEV494M-UEKEV49UM-424fe1561da4-72",
+        scores: [
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+        ]
+    },
+    {
+        name: "Cherry",
+        photo: "https://ca.slack-edge.com/TEKEV494M-UEJJCKPEU-65ae4d2f34a8-72",
+        scores: [
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+        ]
+    },
+    {
+        name: "Peach",
+        photo: "https://ca.slack-edge.com/TEKEV494M-UELBCDTP1-4e9d75f27977-72",
+        scores: [
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+        ]
+    },
+    {
+        name: "Harper",
+        photo: "https://ca.slack-edge.com/TEKEV494M-UEJH05HS4-gf6dd4d8e8b7-72",
+        scores: [
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+        ]
+    },
+    {
+        name: "Frenchie",
+        photo: "https://ca.slack-edge.com/TEKEV494M-UEJH63JNL-1b2047131155-72",
+        scores: [
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+        ]
+    },
+    {
+        name: "Animal",
+        photo: "https://ca.slack-edge.com/TEKEV494M-UEKR36SER-7888951311c7-192",
+        scores: [
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+        ]
+    },
+    {
+        name: "Slayer",
+        photo: "https://ca.slack-edge.com/TEKEV494M-UEKR36SER-7888951311c7-192",
+        scores: [
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+        ]
+    },
+    
+]
+
+
+module.exports = friends;
